567c9d43c199721b115960310dffe4192cd72941
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-09-27 14:04:30 -0700

    fetched api data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  useEffect(() =>{
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}&from=0&to=5&calories=591-722&health=alcohol-free`)
+    const data = await response.json()
+    console.log(data)
+  }
+
   return (
     <div className="App">
       <h1>Recipe App</h1>

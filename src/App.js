3f54b3b33e79c27b900524e87d1dbec9c3db610e
German Nunez <germannunez@Germans-MacBook-Pro.local>
2019-09-27 15:43:30 -0700

    added ingredients from data and removed console logs

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}`)
     const data = await response.json()
-    console.log(data)
     setRecipes(data.hits)
   }
 
@@ -44,6 +43,7 @@ const App = () => {
             title={r.recipe.label}
             calories={r.recipe.calories}
             image={r.recipe.image}
+            ingredients={r.recipe.ingredients}
           />
         ))
       }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return(
         <div>
             <h1>{title}</h1>
             <p>{calories}</p>
             <img src={image}/>
+            <ul>
+                {ingredients.map((ing, i) =>(
+                    <li key={i}>{ing.text}</li>
+                ))}
+            </ul>
         </div>
     )
 }

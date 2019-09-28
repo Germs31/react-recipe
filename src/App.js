df6b414242a549fef4421bdade1ee566e1049c70
German Nunez <germannunez@germans-mbp.home>
2019-09-28 11:56:48 -0700

    styled data into cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Recipe from './Recipe.js'
 import logo from './logo.svg';
 import './App.css';
 
+
 const App = () => {
 
   const [recipes, setRecipes] = useState([])
@@ -23,30 +24,36 @@ const App = () => {
     setSearch('')
   }
 
+  //${query}
+
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}`)
+    const response = await fetch(`https://api.edamam.com/search?q=bacon&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}`)
     const data = await response.json()
     setRecipes(data.hits)
   }
 
   return (
     <div className="App">
-      <h1>Recipe App</h1>
-      <form onSubmit={getSearch}>
-        <input type="text" value={search} onChange={searchVal}/>
-        <button type="submit">Search</button>
-      </form>
-      {
-        recipes.map((r,i) => (
-          <Recipe 
-            key={i}
-            title={r.recipe.label}
-            calories={Math.round(r.recipe.calories)}
-            image={r.recipe.image}
-            ingredients={r.recipe.ingredients}
-          />
-        ))
-      }
+      <div className="header">
+        <h1>Recipe App</h1>
+        <form className="form" onSubmit={getSearch}>
+          <input className="search" type="text" value={search} onChange={searchVal}/>
+          <button className="btn" type="submit">Search</button>
+        </form>
+      </div>
+      <div className="recipes">
+        {
+          recipes.map((r,i) => (
+            <Recipe 
+              key={i}
+              title={r.recipe.label}
+              calories={Math.round(r.recipe.calories)}
+              image={r.recipe.image}
+              ingredients={r.recipe.ingredients}
+            />
+          ))
+        }
+      </div>
     </div>
   );
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import './App.css';
+
 
 const Recipe = ({title, calories, image, ingredients}) => {
     return(
-        <div>
-            <h1>{title}</h1>
-            <p>{calories} calories</p>
-            <img src={image}/>
-            <ul>
-                {ingredients.map((ing, i) =>(
-                    <li key={i}>{ing.text}</li>
-                ))}
-            </ul>
-        </div>
+        
+            <div className="card">
+                <h1>{title}</h1>
+                <p>{calories} calories</p>
+                <img src={image}/>
+                <ul>
+                    {ingredients.map((ing, i) =>(
+                        <li key={i}>{ing.text}</li>
+                    ))}
+                </ul>
+            </div>
     )
 }
 
